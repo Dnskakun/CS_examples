40862ea1e75fc11291b349e674f348a79c17c921
dnskakun <dimon_sdn@list.ru>
2022-12-14 22:02:00 +0300

    Example009 Dog&Friends

---
+++ Example009_Dog_Friends/Program.cs
@@ -0,0 +1,28 @@
+﻿int count = 0;
+int time;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFreindSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFreindSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFreindSpeed) * time;
+    count++;
+    Console.WriteLine(distance);
+}
+
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.Write(" раз.");
